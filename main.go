7407d2574e17d6d8f98adee6df42d2c5337d0b49
eduardomassami <massami.uchida@gmail.com>
2024-09-22 02:33:01 -0300

    add Logger functionality

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/eduardomassami/gopportunities/router"
+import (
+	"github.com/eduardomassami/gopportunities/config"
+	"github.com/eduardomassami/gopportunities/router"
+)
+
+var (
+  logger *config.Logger
+)
 
 func main () {
+
+  logger := config.GetLogger("main")
+
+  err := config.Init()
+  if err != nil {
+    logger.Errorf("config initialization error: %v", err)
+    return
+  }
   router.Initialize()
   print("teste, teste, alguém na escuta?")
 }
