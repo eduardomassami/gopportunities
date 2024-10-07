a3a5c452dc5edcf80b9e5a98b306267557e3f3da
eduardomassami <massami.uchida@gmail.com>
2024-10-06 21:57:16 -0300

    refactor handler

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/eduardomassami/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-  ctx.JSON(http.StatusOK, gin.H{
-    "msg": "LIST Opening",
-  })
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
