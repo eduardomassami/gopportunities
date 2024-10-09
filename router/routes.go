1c247fae37da7282cfa940f0d6ed94e8a63bb85c
eduardomassami <massami.uchida@gmail.com>
2024-10-08 22:18:52 -0300

    init config swagger

--- router/routes.go
+++ router/routes.go
@@ -1,23 +1,30 @@
 package router
 
 import (
+	docs "github.com/eduardomassami/gopportunities/docs"
 	"github.com/eduardomassami/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
-  handler.InitializeHandler()
+	handler.InitializeHandler()
 
-  v1 := router.Group("/api/v1")
-  
-  v1.GET("/opening", handler.ShowOpeningHandler)
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 
-  v1.POST("/opening", handler.CreateOpeningHandler)
+	v1.GET("/opening", handler.ShowOpeningHandler)
 
-  v1.DELETE("/opening", handler.DeleteOpeningHandler)
+	v1.POST("/opening", handler.CreateOpeningHandler)
 
-  v1.PUT("/opening", handler.UpdateOpeningHandler)
+	v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-  v1.GET("/openings", handler.ListOpeningHandler)
+	v1.PUT("/opening", handler.UpdateOpeningHandler)
+
+	v1.GET("/openings", handler.ListOpeningHandler)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
